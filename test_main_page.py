0165fd2fbb431feae2de400be571ab078a6f3b74
ivan <lenovo18472@gmail.com>
2024-10-11 09:14:29 +0300

    selectors have been placed in a separate entity

--- test_main_page.py
+++ test_main_page.py
@@ -1,21 +1,20 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.locators import MainPageLocators
 
-link = "http://selenium1py.pythonanywhere.com/"
- 
+
+link = "http://selenium1py.pythonanywhere.com/" 
 
 def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link = browser.find_element(*MainPageLocators.LOGIN_LINK)
     login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   
     page.open()                      
     page.go_to_login_page()
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
