b7718fb04ff1b877e5bb03ba59223acec6c8d7af
ivan <lenovo18472@gmail.com>
2024-10-11 09:54:22 +0300

    added a transition to the login page and a test for the login page

--- test_main_page.py
+++ test_main_page.py
@@ -5,16 +5,22 @@ from .pages.locators import MainPageLocators
 
 link = "http://selenium1py.pythonanywhere.com/" 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(*MainPageLocators.LOGIN_LINK)
-    login_link.click()
+# def go_to_login_page(browser):
+#     login_link = browser.find_element(*MainPageLocators.LOGIN_LINK)
+#     login_link.click()
+
+# def test_guest_can_go_to_login_page(browser):
+#     page = MainPage(browser, link)   
+#     page.open()                      
+#     page.go_to_login_page()
+
+# def test_guest_should_see_login_link(browser):
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)   
-    page.open()                      
-    page.go_to_login_page()
-
-def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
