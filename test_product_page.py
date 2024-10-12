81fc46d268071acb54e7d321107c4e311dc34dc2
ivan <lenovo18472@gmail.com>
2024-10-12 13:04:59 +0300

    added xfail for a bug on promo=offers 7

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,25 @@
+import pytest
 from .pages.product_page import ProductPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', [
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", 
+                 marks=pytest.mark.xfail),
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+])
 
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser, link)  # Инициализация страницы товара
-    page.open()  # Открываем страницу товара
-    page.add_to_basket()  # Добавляем товар в корзину
-    page.solve_quiz_and_get_code()  # Решаем задачу и получаем код
-    page.should_be_success_message()  # Проверяем, что товар добавлен
-    page.should_be_correct_product_name()  # Проверяем, что название товара в сообщении совпадает
-    page.should_be_correct_basket_price()  # Проверяем, что цена корзины совпадает с ценой товара
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_success_message()
+    page.should_be_correct_product_name()
+    page.should_be_correct_basket_price() 
